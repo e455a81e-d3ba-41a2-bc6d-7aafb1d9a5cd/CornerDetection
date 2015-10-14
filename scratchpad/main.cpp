5ece96c5a1ee3ed038ba03ca9488097646eefe55
Leonhardt Schwarz <if12b076@technikum-wien.at>
2015-10-15 00:58:43 +0200

    Removed several OpenCV Functions

--- scratchpad/main.cpp
+++ scratchpad/main.cpp
@@ -11,41 +11,35 @@ namespace {
     const char* TEST_IMG_DIR = "TestData/checker.jpg";
 }
 
+int test() {
 
+    auto image = cv::imread(TEST_IMG_DIR, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat dst, dstNorm, dstNormScaled;
+    dst = cv::Mat::zeros(image.size(), CV_32FC1);
+    iviso::cornerHarris(image, dst, 3, 3, 0.05);
+    cv::normalize(dst, dstNorm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs( dstNorm, dstNormScaled );
+    cv::namedWindow(WINDOW_NAME, cv::WINDOW_AUTOSIZE);
 
-#define tolerance 0.1e-20
-double square(double i)
-{
-    return pow(i, 2.0);
-}
+    cv::imshow(WINDOW_NAME, dstNormScaled);
+    cv::waitKey(0);
+    return 0;
 
-double max(double a, double b)
-{
-    return a > b ? a : b;
-}
-
-
-void solve_eig(double A, double B, double C, double D,
-               double* lambda1, double *v1x, double*v1y,
-               double* lambda2, double *v2x, double*v2y )
-{
-    if(B*C <= tolerance  ) {
-        *lambda1 = A;
-        *lambda2 = D;
-        return;
+    for (int j = 0; j < dstNormScaled.rows; j++ ) {
+        for (int i = 0; i < dstNormScaled.cols; i++) {
+            if (static_cast<int>(dstNormScaled.at<float>(j,i)) > 200) {
+                //dstNormScaled.at<float>(j,i) = 0;
+                cv::circle(dstNormScaled, cv::Point(i,j), 5, cv::Scalar(0),2, 8, 0);
+            }
+        }
     }
 
-    double tr = A + D;
-    double det = A * D - B * C;
-    double S = sqrt( square(tr/2.0) - det );
-    *lambda1 = tr/2.0 + S;
-    *lambda2 = tr/2.0 - S;
 }
-
 
 int main(int argc, char **argv)
 {
-
+    test();
+        return 0;
     float f[2][2] = {{2,7},{-1,-6}};
     float b[4][4] = {
         { 12, 0, 0, 0},
@@ -57,14 +51,12 @@ int main(int argc, char **argv)
     auto M = cv::Mat(4,4,CV_32FC1,b);
     auto E = cv::Mat(M.size(), CV_32FC1);
 
-    double scale = (double)(1 << ((3 > 0 ? 3 : 3) - 1)) * 3;
-    scale = 1.0/ scale;
-    Sobel( M, E, CV_32F, 0, 1, 3, scale, 0, cv::BORDER_CONSTANT);
+    iviso::cornerHarris(M, E, 3, 3, 0.04);
     std::cout << E << std::endl;
 
-    iviso::cornerHarris(M,E,3,3,0.04);
+    cv::cornerHarris(M, E, 3, 3, 0.04);
+    std::cout << E << std::endl;
 
-    std::cout << E;
     /*
     for (int j = 0; j < 4; j++) {
         for (int i = 0; i < 4; i++) {
@@ -74,11 +66,6 @@ int main(int argc, char **argv)
         std::cout <<  std::endl;
     }
 
-     iviso::cornerHarris(M,E,3,3,0.04);
-     std::cout << E << std::endl;
-
-     cv::cornerHarris(M, E, 3, 3, 0.04);
-     std::cout << E << std::endl;
      return 0;
      /*double lambda1 = 0;
      double v1x = 0;
@@ -87,7 +74,7 @@ int main(int argc, char **argv)
      double lambda2 = 0;
      double v2x = 0;
      double v2y = 0;
-     solve_eig(2,7,-1, -6, &lambda1, &v1x, &v1y, &lambda2, &v2x, &v2y);
+     eigenValues2x2(2,7,-1, -6, &lambda1, &v1x, &v1y, &lambda2, &v2x, &v2y);
 
 
      std::cout << lambda1 << "\t" << lambda2 << std::endl;
@@ -106,27 +93,3 @@ int main(int argc, char **argv)
  */
 }
 
-int test() {
-
-    auto image = cv::imread(TEST_IMG_DIR, CV_LOAD_IMAGE_GRAYSCALE);
-    cv::Mat dst, dstNorm, dstNormScaled;
-    dst = cv::Mat::zeros(image.size(), CV_32FC1);
-    cv::cornerHarris(image, dst, 7, 5, 0.05);
-    cv::normalize(dst, dstNorm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
-    cv::convertScaleAbs( dstNorm, dstNormScaled );
-    cv::namedWindow(WINDOW_NAME, cv::WINDOW_AUTOSIZE);
-
-
-    for (int j = 0; j < dstNormScaled.rows; j++ ) {
-        for (int i = 0; i < dstNormScaled.cols; i++) {
-            if (static_cast<int>(dstNormScaled.at<float>(j,i)) > 200) {
-                //dstNormScaled.at<float>(j,i) = 0;
-                cv::circle(dstNormScaled, cv::Point(i,j), 5, cv::Scalar(0),2, 8, 0);
-            }
-        }
-    }
-
-    cv::imshow(WINDOW_NAME, dstNormScaled);
-    cv::waitKey(0);
-    return 0;
-}
--- src/CornerDetection.cpp
+++ src/CornerDetection.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
 #include "CornerDetection.h"
 
 using namespace cv;
@@ -9,6 +10,7 @@ enum class Direction {
 };
 
 namespace {
+
     int sobel_x_kernel[3][3] =
         {{-1, 0, 1},
          {-2, 0, 2},
@@ -17,6 +19,7 @@ namespace {
         {{-1, -2, -1},
          {0,  0,  0},
          {1,  2,  1}};
+    int box_kernel[3][3] = {{1, 1, 1}, {1, 1, 1}, {1 ,1 ,1}};
 }
 
 template<typename T>
@@ -29,149 +32,122 @@ T CheckBorderGetPixel(const Mat& src, int x, int y) {
 }
 
 
-static void Sobel(const Mat& input, Mat& output, int ddepth, double scale, Direction dir)
+static void convoluteImage(const Mat& input, Mat& output, int ddepth, const Mat& kernel, double scale = 1)
 {
     constexpr int size = 3 / 2;
     output.create(input.size(), ddepth);
-    auto kernel = Mat();
-
-    if (dir == Direction::Y)
-        kernel = Mat(3,3, CV_32S, &sobel_x_kernel);
-    else
-        kernel = Mat(3,3, CV_32S, &sobel_y_kernel);
-
 
     for (int x = 0; x < input.cols ; x++)
     {
         for (int y = 0; y < input.rows; y++) {
-            float accumulator = 0;
+            double accumulator = 0;
 
             for (int kx = -size; kx <= size; kx++) {
                 for (int ky = -size; ky <= size; ky++) {
                     accumulator += scale * CheckBorderGetPixel<float>(input, x + kx, y + ky) * kernel.at<int>(kx + size, ky + size);
 
                 }
-
             }
             //accumulator = floorf(accumulator * 10000000) / 10000000;
-            output.at<float>(y,x) = accumulator;
+            output.at<float>(y,x) = static_cast<float>(accumulator);
         }
 
     }
 }
 
+static void applyBoxFilter(const Mat& input, Mat& output, int ddepth)
+{
+    auto kernel = Mat(3,3, CV_32S, &box_kernel);
+
+    //convoluteImage(input, output, ddepth, kernel, 1);
+    filter2D(input, output, ddepth, kernel, Point(-1, -1), 0);
+}
+
+static void Sobel(const Mat& input, Mat& output, int ddepth, double scale, Direction dir) {
+
+    auto kernel = Mat();
+    if (dir == Direction::Y)
+        kernel = Mat(3,3, CV_32S, &sobel_x_kernel);
+    else
+        kernel = Mat(3,3, CV_32S, &sobel_y_kernel);
+
+    convoluteImage(input, output, ddepth, kernel, scale);
+
+}
+
+void eigenValues2x2(const Vec3f& input, Vec2f& output)
+{
+    const double& A = input[0];
+    const double& B = input[1];
+    const double& C = input[2];
+
+    double u = (A + C)*0.5;
+    double v = std::sqrt((A - C)*(A - C)*0.25 + B*B);
+    double l1 = u + v;
+    double l2 = u - v;
+    output[0] = static_cast<float>(l1);
+    output[1] = static_cast<float>(l2);
+}
+
 static void calcEigenVals( const cv::Mat& input, cv::Mat& output, int blockSize, int apertureSize)
 {
-    double scale = (double)(1 << ((apertureSize > 0 ? apertureSize : 3) - 1)) * blockSize;
+
+    double scale = static_cast<double>((1 << ((apertureSize > 0 ? apertureSize : 3) - 1)) * blockSize);
     scale = 1.0/ scale;
 
     auto size = static_cast<Size>(input.size());
 
     auto Dx = Mat();
     auto Dy = Mat();
+
+
     Sobel(input, Dx, CV_32F, scale, Direction::X);
     Sobel(input, Dy, CV_32F, scale, Direction::Y);
 
-    for (int j = 0; j < size.height; j++) {
-        for (int i = 0; i < size.width; i++) {
+    auto cov = Mat(size, CV_32FC3);
+    for( int i = 0; i < size.height; i++ ) {
+        for(int j = 0; j < size.width; j++ )
+        {
+            float dx = Dx.at<float>(i,j);
+            float dy = Dy.at<float>(i,j);
 
+            cov.at<Vec3f>(i,j)[0] = dx*dx;
+            cov.at<Vec3f>(i,j)[1] = dx*dy;
+            cov.at<Vec3f>(i,j)[2] = dy*dy;
         }
     }
 
+    applyBoxFilter(cov, cov, cov.depth());
 
+    //boxFilter(cov, cov, cov.depth(), Size(blockSize, blockSize), Point(-1,-1), false,  BORDER_DEFAULT);
+
+    for (int j = 0; j < size.height; j++) {
+
+        for (int i = 0; i < size.width; i++) {
+            const auto& row = cov.at<Vec3f>(j, i);
+            eigenValues2x2(row, output.at<Vec2f>(j, i));
+        }
+    }
 }
 
 void iviso::cornerHarris(const Mat &input, Mat &output, int blockSize, int apertureSize, double k, int borderType)
 {
 
     //MatExpr is a "hidden" proxy class therefore explicit cast see Scott Meyers "Effective Modern C++" Item 6
-    auto EigenVals = static_cast<Mat>(Mat::zeros(input.size(), CV_32FC(6)));
+    auto EigenVals = static_cast<Mat>(Mat::zeros(input.size(), CV_32FC(2)));
+    Mat tmp;
+    input.convertTo(tmp, CV_32F);
     output.create(input.size(), CV_32FC1);
 
-    cornerEigenValsAndVecs(input, EigenVals, blockSize, apertureSize, BORDER_DEFAULT);
+    calcEigenVals(tmp, EigenVals, blockSize, apertureSize);
 
     for (int j = 0; j < input.rows; j++) {
         for (int i = 0; i < input.cols; i++) {
-            auto lambda1 = EigenVals.at<Vec6f>(j,i)[0];
-            auto lambda2 = EigenVals.at<Vec6f>(j,i)[1];
-            //High values of k may not fit into float type.
+            auto lambda1 = EigenVals.at<Vec2f>(j,i)[0];
+            auto lambda2 = EigenVals.at<Vec2f>(j,i)[1];
             output.at<float>(j,i) = lambda1*lambda2 - static_cast<float>(k)*pow((lambda1 + lambda2), 2.0f);
         }
     }
 }
 
-static void eigen2x2( const float* cov, float* dst, int n )
-{
-    for( int j = 0; j < n; j++ )
-    {
-        double a = cov[j*3];
-        double b = cov[j*3+1];
-        double c = cov[j*3+2];
 
-        double u = (a + c)*0.5;
-        double v = std::sqrt((a - c)*(a - c)*0.25 + b*b);
-        double l1 = u + v;
-        double l2 = u - v;
-
-        double x = b;
-        double y = l1 - a;
-        double e = fabs(x);
-
-        if( e + fabs(y) < 1e-4 )
-        {
-            y = b;
-            x = l1 - c;
-            e = fabs(x);
-            if( e + fabs(y) < 1e-4 )
-            {
-                e = 1./(e + fabs(y) + FLT_EPSILON);
-                x *= e, y *= e;
-            }
-        }
-
-        double d = 1./std::sqrt(x*x + y*y + DBL_EPSILON);
-        dst[6*j] = (float)l1;
-        dst[6*j + 2] = (float)(x*d);
-        dst[6*j + 3] = (float)(y*d);
-
-        x = b;
-        y = l2 - a;
-        e = fabs(x);
-
-        if( e + fabs(y) < 1e-4 )
-        {
-            y = b;
-            x = l2 - c;
-            e = fabs(x);
-            if( e + fabs(y) < 1e-4 )
-            {
-                e = 1./(e + fabs(y) + FLT_EPSILON);
-                x *= e, y *= e;
-            }
-        }
-
-        d = 1./std::sqrt(x*x + y*y + DBL_EPSILON);
-        dst[6*j + 1] = (float)l2;
-        dst[6*j + 4] = (float)(x*d);
-        dst[6*j + 5] = (float)(y*d);
-    }
-}
-
-
-static void calcEigenValsVecs( const cv::Mat& _cov, cv::Mat& _dst )
-{
-    cv::Size size = _cov.size();
-    if( _cov.isContinuous() && _dst.isContinuous() )
-    {
-        size.width *= size.height;
-        size.height = 1;
-    }
-
-    for( int i = 0; i < size.height; i++ )
-    {
-        const float* cov = _cov.ptr<float>(i);
-        float* dst = _dst.ptr<float>(i);
-
-        eigen2x2(cov, dst, size.width);
-    }
-}
